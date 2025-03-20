35b6721b528b578b7edec650de36f2068b7d83ea
KyokaKyoka <752572249@qq.com>
2025-03-20 01:08:36 +0000

    #37 fixed bugs

--- backend/src/main/java/com/tribytegenius/CareerCompass/dto/JobDTO.java
+++ backend/src/main/java/com/tribytegenius/CareerCompass/dto/JobDTO.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobDTO {
+    private String id;
     private String name;
     private String company;
     private String type;
