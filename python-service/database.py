93ae4b8f8ed9c94e263896e54cc4e13f97c735c1
Lian Guo <lianguo@Lians-MacBook-Air.local>
2025-04-06 03:44:02 +0100

    add k8s deployment file and local test

--- python-service/database.py
+++ python-service/database.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+DB_USER = os.getenv("DATABASE_USERNAME")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DB_HOST = os.getenv("DB_ENDPOINT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
