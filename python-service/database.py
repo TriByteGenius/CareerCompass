86a02da23467f1a328a7aa1c2fcd9a6662d28797
Lian Guo <lianguo@Lians-MacBook-Air.local>
2025-04-06 13:27:00 +0100

    finish eks deploy file and test ok

--- python-service/database.py
+++ python-service/database.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 # SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-DB_USER = os.getenv("DATABASE_USERNAME")
+DB_USERNAME = os.getenv("DATABASE_USERNAME")
 DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DB_HOST = os.getenv("DB_ENDPOINT")
+DB_ENDPOINT = os.getenv("DB_ENDPOINT")
 DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_ENDPOINT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
